803ab309543a85870013e55cf506b5f9a9d7cec3
james thompson <jamesantonthompson@gmail.com>
2017-09-16 06:34:30 +1000

    final

--- init.bat
+++ init.bat
@@ -7,3 +7,8 @@ npm install --save-dev onchange parallelshell
 npm install --save-dev rimraf
 npm -g install copyfiles
 npm install --save-dev usemin-cli cssmin uglifyjs htmlmin
+npm install -g grunt-cli
+npm install grunt --save-dev
+npm install grunt-sass --save-dev
+npm install time-grunt --save-dev
+npm install jit-grunt --save-dev
